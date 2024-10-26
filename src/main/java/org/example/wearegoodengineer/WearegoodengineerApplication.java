26458e084f8aca582da90dea642931154942f7ef
xumaolin <rirto0627@gmail.com>
2024-10-27 03:25:37 +0800

    新增SwaggerUI

--- src/main/java/org/example/wearegoodengineer/WearegoodengineerApplication.java
+++ src/main/java/org/example/wearegoodengineer/WearegoodengineerApplication.java
@@ -1,10 +1,12 @@
 package org.example.wearegoodengineer;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "My API", version = "1.0", description = "API for OpenAI travel planning"))
 public class WearegoodengineerApplication {
 
     public static void main(String[] args) {
