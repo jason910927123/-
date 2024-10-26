8b311fd94d23c90b27a59b1fdd40b2dc037bb3ac
xumaolin <rirto0627@gmail.com>
2024-10-27 03:28:55 +0800

    新增 UI註解

--- src/main/java/org/example/wearegoodengineer/WearegoodengineerApplication.java
+++ src/main/java/org/example/wearegoodengineer/WearegoodengineerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+//http://localhost:8080/swagger-ui.html 訪問ui api
+//http://localhost:8080/v3/api-docs 訪問api文檔
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "My API", version = "1.0", description = "API for OpenAI travel planning"))
 public class WearegoodengineerApplication {
